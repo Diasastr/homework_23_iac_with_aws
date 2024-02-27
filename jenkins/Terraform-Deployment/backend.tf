57dd68152bd6632234f9b1fde76c7a9264c0b7ee
strebkova.diana7@gmail.com <strebkova.diana7@gmail.com>
2024-02-27 01:06:21 +0100

    Deleted backend for terraform-deployment for ease

--- jenkins/Terraform-Deployment/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "diana-terraform-state-bucket-robotdreams"
-    key            = "envs/jenkins/terraform.tfstate"
-    region         = "eu-north-1"
-    encrypt        = true
-  }
-}
